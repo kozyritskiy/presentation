ad7da5e8489be070cd5bd4617bfa663e4d597098
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-12-08 16:20:46 +0300

    add presentation

---
+++ test/test-markdown-external.js
@@ -0,0 +1,16 @@
+Reveal.addEventListener( 'ready', function() {
+
+	QUnit.module( 'Markdown' );
+
+	QUnit.test( 'Vertical separator', function( assert ) {
+		assert.strictEqual( document.querySelectorAll( '.reveal .slides>section>section' ).length, 2, 'found two slides' );
+	});
+
+	QUnit.test( 'Language highlighter', function( assert ) {
+		assert.strictEqual( document.querySelectorAll( '.hljs-keyword' ).length, 1, 'got rendered highlight tag.' );
+		assert.strictEqual( document.querySelector( '.hljs-keyword' ).innerHTML, 'var', 'the same keyword: var.' );
+	});
+
+} );
+
+Reveal.initialize();
